8f8d75d554a92aeb84fedbb554b3ad157521e5fc
unknown <swathy.dhanesh00@gmail.com>
2020-10-14 21:35:37 +0530

    student and teacher landing screen created

--- app/src/main/java/com/example/fireadmission/LoginActivity.java
+++ app/src/main/java/com/example/fireadmission/LoginActivity.java
@@ -1,14 +1,32 @@
 package com.example.fireadmission;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private RadioGroup radioGroup;
+    private RadioButton userType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+    }
+
+    public void login(View view){
+        radioGroup = (RadioGroup) findViewById(R.id.radioGrp);
+        int selectedID = radioGroup.getCheckedRadioButtonId();
+        if(selectedID == R.id.radioButton2)
+            setContentView(R.layout.activity_student);
+        else
+            setContentView(R.layout.activity_teacher);
     }
 }
